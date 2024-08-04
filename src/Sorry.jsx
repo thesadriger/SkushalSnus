595f4f9cb748253fd85b53c8bc248da45ff1f842
thesadriger <thesadriger@gmail.com>
2024-08-05 00:57:45 +0300

    Create Sorry.jsx

---
+++ src/Sorry.jsx
@@ -0,0 +1,18 @@
+import Oops from './Components/Titles/Oops'
+
+export default function Sorry() {
+  return ( 
+    <>
+      <main>
+        <>
+          <Oops/>
+          <div style={{ textAlign: 'center', marginTop: '20px' }}>
+            <a href="https://myshop-bcw638.myinsales.ru/" target="_blank" rel="noopener noreferrer">
+              <img src="/logotype-snslavka_black.svg" alt="shop" style={{ width: '50px', height: '50px' }} />
+            </a>
+          </div>
+        </>
+      </main>
+    </>
+  )
+}

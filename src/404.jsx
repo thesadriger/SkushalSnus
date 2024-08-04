94b372b7f171a191f765d418cef166c035368650
thesadriger <thesadriger@gmail.com>
2024-08-05 00:57:39 +0300

    Create 404.jsx

---
+++ src/404.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Sorry from './Sorry.jsx'
+import './index.css'
+
+
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(<Sorry/>)

f39231e4ce80961daf7d990d5dc7c00eea57199d
thesadriger <thesadriger@gmail.com>
2024-08-05 00:57:41 +0300

    Create Oops.jsx

---
+++ src/Components/Titles/Oops.jsx
@@ -0,0 +1,15 @@
+import './NPStyle.css'
+
+
+export default function Oops() {
+    return(
+        <section className="header-title">
+            <div className="title-wrapper">
+                <h1 className="sweet-title">
+                    <span data-text="Упс!">Упс!</span>
+                    <span data-text="Приложение в работе">Приложение в работе</span>
+                </h1>
+            </div>
+        </section>
+    )
+}

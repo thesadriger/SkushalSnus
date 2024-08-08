7be4e9e27280c545f9d82357b4956d1f8d6f2a45
thesadriger <thesadriger@gmail.com>
2024-08-08 13:21:54 +0300

    Update TabSection.jsx

--- src/Components/TabSection.jsx
+++ src/Components/TabSection.jsx
@@ -4,20 +4,30 @@ import { styled } from 'styled-components'
 import MenuButtons from './MenuButtons/MenuButtons'
 import { menuBtn } from './data'
 
+const MenuSection = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.5rem;
+    background-color: #f8f9fa; /* Пример фона */
+`;
+
 
 export default function TabSection({ active, onChange }) {
     return(
-        <section style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '2rem' }}>
-            <MenuButtons isActive = {active === 'main' } onClick={() => onChange('main')}>Магазин</MenuButtons>
+        <MenuSection>
+            <MenuButtons isActive = {active === 'main' } onClick={() => onChange('main')}>Главная</MenuButtons>
             <MenuButtons isActive = {active === 'store' } onClick={() => onChange('store')}>Корзина
-                <img
+                {/* <img
                     src={menuBtn.shop.image}
                     alt="Корзина"
                     style={{ width: '10rem', height: '10rem', marginRight: '1.5rem', marginTop: '3rem', position: 'flex', background: 'var(--product-background)' }}
-                />
+                /> */}
             </MenuButtons>
-            <MenuButtons isActive = {active === 'shop' } onClick={() => onChange('shop')}>Магазин</MenuButtons>
-        </section>
+            <MenuButtons isActive = {active === 'collection' } onClick={() => onChange('collection')}>Каталог</MenuButtons>
+            <MenuButtons isActive = {active === 'profile' } onClick={() => onChange('profile')}>Профиль</MenuButtons>
+            <MenuButtons isActive = {active === 'shop' } onClick={() => onChange('shop')}>Карта</MenuButtons>
+        </MenuSection>
     )
 }
 

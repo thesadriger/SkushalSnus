3716e0fe9e4e7a2b6979fb3b8e768a3e2865996f
thesadriger <thesadriger@gmail.com>
2024-08-08 13:21:52 +0300

    Update ShopSection.jsx

--- src/Components/ShopSection.jsx
+++ src/Components/ShopSection.jsx
@@ -1,7 +1,12 @@
+import Vitrina from './Catalog/Vitrina/Vitrina'
+
 export default function ShopSection() {
     return (
         <section>
             <h3>Ассортимент</h3>
+            <section>
+                <Vitrina/>
+            </section>
         </section>
     )
 }

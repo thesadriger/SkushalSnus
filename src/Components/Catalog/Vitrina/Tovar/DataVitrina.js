df42b741c775e3a01d2937a55121c984a32a687e
thesadriger <thesadriger@gmail.com>
2024-08-08 13:21:34 +0300

    Create DataVitrina.js

---
+++ src/Components/Catalog/Vitrina/Tovar/DataVitrina.js
@@ -0,0 +1,34 @@
+export const vitrinaItem = {
+    fedrs: {
+      // logo: './large_BANAN_BOMBA_logo.png',
+      image: './FEDRS_logotype_white.png',
+      title: 'FEDRS',
+      description: 'Апельсиновые конфеты, 125 мг', 
+      price: '410.00 руб',
+      // rating: '',
+    },
+    arqa: {
+      // logo: './large_BERRY_ON_BOARD_logo.png',
+      image: './ARQA_logotype_white.png',
+      title: 'ARQA',
+      description: 'Апельсиновые конфеты, 125 мг', 
+      price: '',
+      rating: '',
+    },
+    shok: {
+      // logo: './large_SUMMER_logo.png',
+      image: './large_SUMMER.png',
+      title: 'SHOK',
+      description: 'Апельсиновые конфеты, 125 мг', 
+      price: '',
+      rating: '',
+    },
+    corvus: {
+      // logo: './large_SUMMER_logo.png',
+      image: './large_SUMMER.png',
+      title: 'CORVUS',
+      description: 'Апельсиновые конфеты, 125 мг', 
+      price: '',
+      rating: '',
+    },
+  };
